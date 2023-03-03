323aaf8fe5dae6dc8a46bb8123b9534fa95f27e5
wbforever <2505237118@qq.com>
2023-03-03 21:08:20 +0800

    C语言练习

---
+++ wbn.c
@@ -0,0 +1,120 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+//int main()
+//{
+//    int M = 0;
+//    scanf("%d", &M);
+//    if (M % 5 == 0)
+//    {
+//        printf("YES");
+//    }
+//    else
+//    {
+//        printf("NO");
+//    }
+//    return 0;
+//}
+//int main()
+//{
+//	int x = 0;
+//	scanf("%d", &x);
+//	if (x < 0)
+//	{
+//		int y = 1;
+//		printf("%d", y);
+//	}
+//	else if (x == 0)
+//	{
+//		int y = 0;
+//			printf("%d", y);
+//	}
+//	else
+//	{
+//		int y = -1;
+//			printf("%d", y);
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a = 40;
+//	int c = 212;
+//	int result = 0;
+//	result = (-8 + 22) * a - 10 + (c / 2);
+//	printf("%d", result);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	int c = 0;
+//	int d = 0;
+//	scanf("%d %d", &a, &b);
+//	c = a / b;
+//	d = a % b;
+//	printf("%d %d", c, d);
+//	return 0;
+//}
+//int main()
+//{
+//	printf("Name    Age    Gender\n");
+//	printf("---------------------\n");
+//	printf("Jack    18    man\n");
+//	return 0;
+//}
+//#include <stdio.h>
+//int get_max(int x, int y);
+//{
+//	if (x > y)
+//		return x;
+//	else
+//		return y;
+//}
+//int main()
+//{
+//	int num1 = 0;
+//	int num2 = 0;
+//	int Max = 0;
+//	scanf("%d%d", &num1, &num2);
+//	Max = get_max(num1, num2);
+//	printf("%d", Max);
+//	return 0;
+//}
+//
+//
+//int get_max(int x, int y)
+//
+//{
+//
+//    if (x > y)
+//
+//        return x;
+//
+//    else
+//
+//        return y;
+//
+//}
+
+//int main()
+//
+//{
+//
+//    int num1 = 0;
+//    int num2 = 0;
+//    scanf("%d%d", &num1, &num2);
+//    int max = get_max(num1, num2);
+//    printf("%d\n", max);
+//    return 0;
+//}
+//int main()
+//{
+//	int a = 0;
+//	scanf("%d", &a);
+//	if (a >= 140)
+//		printf("Genius");
+// return 0;
+//}
