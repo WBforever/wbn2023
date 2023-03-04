4df7b762ac3607de1a746cc4961803960ff1dbcc
wbforever <2505237118@qq.com>
2023-03-04 22:19:12 +0800

    03_04代码练习

---
+++ wbn3_04.c
@@ -0,0 +1,59 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+//int main()
+//{
+//    int a = 1;
+//    int b = 1;
+//    while (scanf("%d %d", &a, &b) != EOF)
+//    {
+//        if (a == b)
+//        {
+//            printf("%d=%d\n", a, b);
+//        }
+//        else if (a > b)
+//        {
+//            printf("%d>%d\n", a, b);
+//        }
+//        else
+//            printf("%d<%d\n", a, b);
+//    }
+//    return 0;
+//}
+
+//int main()
+//{
+//    int a = 0;
+//    scanf("%d", &a);
+//    while (a)
+//    {
+//        printf("%d", a % 10);
+//        a = a / 10;
+//    }
+//    return 0;
+//}
+
+//int main()
+//{
+//    float a, b, c, sum, avg;
+//    scanf("%f %f %f", &a, &b, &c);
+//    sum = a + b + c;
+//    avg = sum / 3.0;
+//    printf("%.2f %.2f", sum, avg);
+//    return 0;
+//}
+//
+//int max3(int a, int b, int c)
+//{
+//    return a > b ? a > c ? a : c : b > c ? b : c;
+//}
+//int main()
+//{
+//    int a = 0;
+//    int b = 0;
+//    int c = 0;
+//    float m = 0;
+//    scanf("%d %d %d", &a, &b, &c);
+//    m = 1.0 * max3(a + b, b, c) / ((max3(a, b + c, c) + max3(a, b, b + c));
+//    printf("%.2f", m);
+//    return 0;
+//}
