582006e6c7516a17e64acaf641131bd1c3072ee8
wbforever <2505237118@qq.com>
2023-03-06 21:34:17 +0800

    3-06代码练习

---
+++ wbn_3_06.c
@@ -0,0 +1,35 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+//int main(void)
+//{
+//    int i;
+//    while ((scanf("%d", &i)) != EOF)
+//    {
+//        for (int n = 0; n < i; n++)
+//        {
+//            printf("*");
+//            if (n == i - 1)printf("\n");
+//        }
+//    }
+//    return 0;
+//}
+//
+//int main()
+//{
+//    double arr[5][5];
+//    for (int i = 0; i < 5; ++i)
+//    {
+//        scanf("%lf %lf %lf %lf %lf", &arr[i][0], &arr[i][1], &arr[i][2], &arr[i][3], &arr[i][4]);
+//    }
+//    for (int i = 0; i < 5; ++i)
+//    {
+//        double sum = 0;
+//        for (int j = 0; j < 5; ++j)
+//        {
+//            printf("%.1f ", arr[i][j]);
+//            sum += arr[i][j];
+//        }
+//        printf("%.1f\n", sum);
+//    }
+//    return 0;
+//}
