d05e07a4624b7300291f4cc1f388bcb6880314df
wbforever <2505237118@qq.com>
2023-03-08 22:58:55 +0800

    3_08代码练习

---
+++ wbn_3_08.c
@@ -0,0 +1,60 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+//int main()
+//{
+//	double arr[5];
+//	for (i = 0; i < 5; i++)
+//	{
+//		scanf("%lf %lf %lf %lf %lf", &);
+//	}
+//	
+//}
+//int main()
+//{
+//    int i = 0;
+//    int j = 0;
+//    for (i = 0; i < 5; i++)
+//    {
+//        float score = 0.0;
+//        float sum = 0.0;
+//        for (j = 0; j < 5; j++)
+//        {
+//            scanf("%f", &score);
+//            sum += score;
+//            printf("%.1f ", score);
+//        }
+//        printf("%.1f\n", sum);
+//    }
+//    return 0;
+//}
+
+
+//int main()
+//{
+//    double arr[5][5];
+//    for (int i = 0; i < 5; ++i)
+//    {
+//        scanf("%lf %lf %lf %lf %lf", &arr[i][0], &arr[i][1], &arr[i][2], &arr[i][3], &arr[i][4]);
+//    }
+//    for (int i = 0; i < 5; ++i)
+//    {
+//        double sum = 0;
+//        for (int j = 0; j < 5; ++j)
+//        {
+//            printf("%.1f ", arr[i][j]);
+//            sum += arr[i][j];
+//        }
+//        printf("%.1f\n", sum);
+//    }
+//    return 0;
+//}
+//int main()
+// {
+//    double num1, num2, num3;
+//    scanf("%lf %lf %lf", &num1, &num2, &num3);
+//    double sum = num1 + num2 + num3;
+//    double avg = sum / 3;
+//    printf("%.2lf %.2lf", sum, avg);
+//    return 0;
+//}
+
