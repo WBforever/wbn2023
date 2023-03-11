4768e722113d0a05ad34633454858d977e691547
wbforever <2505237118@qq.com>
2023-03-11 23:47:44 +0800

    3_111代码

---
+++ wbn_3_11.c
@@ -0,0 +1,37 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+//int main()
+//{
+//	int y = 0;
+//	int count = 0;
+//	for (y = 1000; y <= 2000; y++)
+//	{
+//		if ((y % 4 == 0 && y%100!=0) || (y % 400 == 0))
+//		{
+//			count++;
+//			printf("%d ", y);
+//		}
+//	}
+//	printf("\ncount = %d\n", count);
+//	return 0;
+//}
+// 
+//int main()
+//{
+//	int y = 0;
+//	for (y = 1000; y <= 2000; y++)
+//	{
+//		if (y % 4 == 0)
+//		{
+//			if (y % 100 != 0)
+//			{
+//				printf("%d ", y);
+//			}
+//		}
+//		if (y % 400 == 0)
+//		{
+//			printf("%d ", y);
+//		}
+//	}
+//	return 0;
+//}
