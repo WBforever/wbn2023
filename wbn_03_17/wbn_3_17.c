100c248af72d2f4f4354e6e6bc8833b892d70840
wbforever <2505237118@qq.com>
2023-03-17 22:00:25 +0800

    3_17代码

---
+++ wbn_03_17/wbn_3_17.c
@@ -0,0 +1,48 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+//int main()
+//{
+//	int arr[200] = { 1,2,3,4,5 };
+//	printf("%d", sizeof(arr));
+//	return 0;
+//}
+
+
+//√∞≈›≈≈–Ú
+void bubble_sort(int arr[], int sz)
+{
+	int i = 0;
+	for (i = 0; i < sz - 1; i++)
+	{
+		int flag = 1;
+		int j = 0;
+		for (j = 0; j < sz - 1 - i; j++)
+		{
+			if (arr[j] > arr[j + 1])
+			{
+				flag = 0;
+				int tmp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = tmp;
+			}
+		}
+		if (flag == 1)
+		{
+			break;
+		}
+	}
+
+}
+
+int main()
+{
+	int arr[10] = { 7,8,4,5,9,6,1,2,3,0 };
+	int n = 0;
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	bubble_sort(arr, sz);
+	for (n = 0; n < sz ; n++)
+	{
+		printf("%d ", arr[n]);
+	}
+	return 0;
+}
